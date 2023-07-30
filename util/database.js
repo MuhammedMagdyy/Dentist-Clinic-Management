a90cc9e33615c45670a5351f99cd0fb7d89f1aee
MuhammedMagdyy <Mohamedmody04@gmail.com>
2023-07-30 16:43:10 +0300

    returned to mysql

--- util/database.js
+++ util/database.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-      required: true,
-    },
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 module.exports = sequelize;
