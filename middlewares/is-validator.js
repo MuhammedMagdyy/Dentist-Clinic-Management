b8fa212fd256cbd2240eed727d1e2b723d03cb3f
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-29 22:41:25 +0300

    Refactor: Validator middleware
    - Enhanced retruned errors with status code and array of errors.

--- middlewares/is-validator.js
+++ middlewares/is-validator.js
@@ -3,7 +3,9 @@ const { validationResult } = require('express-validator');
 function validationMiddleWare(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new Error('Validation failed, entered data is incorrect.');
+    return res
+      .status(422)
+      .json({ errors: errors.array().map(error => error.msg) });
   }
   next();
 }
