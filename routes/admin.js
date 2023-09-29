55cbac6c947b9ef714cc6ccd7f60e967384ef356
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-29 22:43:36 +0300

    Refactor: Admin routes
    - Applied validation on admin routes.

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,71 @@
 const express = require('express');
+
 const router = express.Router();
 const adminController = require('../controllers/admin');
 const isAuth = require('../middlewares/is-auth');
 const isAdmin = require('../middlewares/is-admin');
 const isNurse = require('../middlewares/is-nurse');
-const { body } = require('express-validator');
-const { validationMiddleWare } = require('../middlewares/is-validator');
+const {
+  getClinicValidator,
+  addClinicValidator,
+  updateClinicValidator,
+  deleteClinicValidator,
+} = require('../utils/validators/admin/clinic/clinicValidator');
+const {
+  getRoleValidator,
+  addRoleValidator,
+  updateRoleValidator,
+  deleteRoleValidator,
+} = require('../utils/validators/admin/role/roleValidator');
+const {
+  getUserValidator,
+  addUserValidator,
+  updateUserValidator,
+  deleteUserValidator,
+} = require('../utils/validators/admin/user/userValidator');
+const {
+  getDentistValidator,
+  addDentistValidator,
+  updateDentistValidator,
+  deleteDentistValidator,
+} = require('../utils/validators/admin/dentist/dentistValidator');
 
 // GET [/admin/]
-router.get('/all-clinics', isAuth, isNurse, adminController.getAllClinics);
-router.get('/clinic/:id', isAuth, isNurse, adminController.getClinic);
+router.get('/clinics', isAuth, isNurse, adminController.getAllClinics);
+router.get(
+  '/clinics/:id',
+  isAuth,
+  isNurse,
+  getClinicValidator,
+  adminController.getClinic
+);
 
-router.get('/all-roles', isAuth, isAdmin, adminController.getAllRoles);
-router.get('/role/:id', isAuth, isAdmin, adminController.getRole);
+router.get('/roles', isAuth, isAdmin, adminController.getAllRoles);
+router.get(
+  '/roles/:id',
+  isAuth,
+  isAdmin,
+  getRoleValidator,
+  adminController.getRole
+);
 
-router.get('/all-users', isAuth, isAdmin, adminController.getAllUsers);
-router.get('/user/:id', isAuth, isAdmin, adminController.getUser);
+router.get('/users', isAuth, isAdmin, adminController.getAllUsers);
+router.get(
+  '/users/:id',
+  isAuth,
+  isAdmin,
+  getUserValidator,
+  adminController.getUser
+);
 
-router.get('/all-dentists', isAuth, isNurse, adminController.getAllDentists);
-router.get('/dentist/:id', isAuth, isNurse, adminController.getDentist);
+router.get('/dentists', isAuth, isNurse, adminController.getAllDentists);
+router.get(
+  '/dentists/:id',
+  isAuth,
+  isNurse,
+  getDentistValidator,
+  adminController.getDentist
+);
 router.get('/dentistId/:id', isAuth, isNurse, adminController.getDentistId);
 router.get('/in-doctor', isAuth, isNurse, adminController.getInDoctor);
 router.get('/out-doctor', isAuth, isNurse, adminController.getOutDoctor);
@@ -27,68 +74,101 @@ router.get('/clinicId/:id', isAuth, isNurse, adminController.getClinicId);
 
 // POST [/admin/]
 router.post(
-  '/add-clinic',
-  // need it to have space in the name
-  [body('name').trim().notEmpty().isString()],
-  validationMiddleWare,
+  '/clinics',
   isAuth,
   isAdmin,
+  addClinicValidator,
   adminController.addClinic
 );
 
 router.post(
-  '/add-role',
-  [body('name').trim().notEmpty()],
-  validationMiddleWare,
+  '/roles',
   isAuth,
   isAdmin,
+  addRoleValidator,
   adminController.addRole
 );
 
 router.post(
-  '/add-user',
-  [
-    body('name').trim().notEmpty().isString(),
-    body('email').isEmail().normalizeEmail().trim().notEmpty(),
-    body('password').trim().isLength({ min: 5 }).notEmpty(),
-  ],
-  validationMiddleWare,
+  '/users',
   isAuth,
   isAdmin,
+  addUserValidator,
   adminController.addUser
 );
 
 router.post(
-  '/add-dentist',
-  [
-    body('name').trim().notEmpty().isString(),
-    body('email').isEmail().normalizeEmail().trim().notEmpty(),
-    body('national_id').trim().notEmpty().isNumeric().isLength({ min: 14 }),
-    body('phone').trim().notEmpty().isNumeric(),
-    body('works_in').trim().notEmpty(),
-  ],
-  validationMiddleWare,
+  '/dentists',
   isAuth,
   isAdmin,
+  addDentistValidator,
   adminController.addDentist
 );
 
 // PUT [/admin/]
-router.put('/clinic/:id', isAuth, isAdmin, adminController.editClinic);
+router.put(
+  '/clinics/:id',
+  isAuth,
+  isAdmin,
+  updateClinicValidator,
+  adminController.editClinic
+);
 
-router.put('/role/:id', isAuth, isAdmin, adminController.editRole);
+router.put(
+  '/roles/:id',
+  isAuth,
+  isAdmin,
+  updateRoleValidator,
+  adminController.editRole
+);
 
-router.put('/user/:id', isAuth, isAdmin, adminController.editUser);
+router.put(
+  '/users/:id',
+  isAuth,
+  isAdmin,
+  updateUserValidator,
+  adminController.editUser
+);
 
-router.put('/dentist/:id', isAuth, isAdmin, adminController.editDentist);
+router.put(
+  '/dentists/:id',
+  isAuth,
+  isAdmin,
+  updateDentistValidator,
+  adminController.editDentist
+);
 
 // DELETE [/admin/]
-router.delete('/clinic/:id', isAuth, isAdmin, adminController.deleteClinic);
+router.delete(
+  '/clinics/:id',
+  isAuth,
+  isAdmin,
+  deleteClinicValidator,
+  adminController.deleteClinic
+);
 
-router.delete('/role/:id', isAuth, isAdmin, adminController.deleteRole);
+router.delete(
+  '/roles/:id',
+  isAuth,
+  isAdmin,
+  deleteRoleValidator,
+  adminController.deleteRole
+);
 
-router.delete('/user/:id', isAuth, isAdmin, adminController.deleteUser);
+router.delete(
+  '/users/:id',
+  isAuth,
+  isAdmin,
+  deleteUserValidator,
+  adminController.deleteUser
+);
 
-router.delete('/dentist/:id', isAuth, isAdmin, adminController.deleteDentist);
+router.delete(
+  '/dentists/:id',
+  isAuth,
+  isAdmin,
+  deleteDentistValidator,
+  adminController.deleteDentist
+);
 
 module.exports = router;
