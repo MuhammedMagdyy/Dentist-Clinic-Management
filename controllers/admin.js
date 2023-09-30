2f093bf3cd05f9de23ad306a6b7dfc020004ab81
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-30 21:54:03 +0300

    Refactor: Admin Controller
    - Applied destructing.
    - Enhanced functions.
    - Ordered importing.

--- controllers/admin.js
+++ controllers/admin.js
@@ -29,8 +29,7 @@ exports.getClinic = async (req, res) => {
       include: [
         {
           model: Specialized,
-          // Dont forget to convert plant to plan
-          attributes: ['id', 'examination', 'treatment_plant'],
+          attributes: ['id', 'examination', 'treatmentPlan'],
           include: [
             {
               model: Patient,
@@ -114,7 +113,7 @@ exports.getUser = async (req, res) => {
         { model: Role, attributes: ['name'] },
         {
           model: Patient,
-          attributes: { exclude: ['createdAt', 'updatedAt', 'created_by'] },
+          attributes: { exclude: ['createdAt', 'updatedAt', 'createdBy'] },
         },
       ],
     });
@@ -140,7 +139,7 @@ exports.getDentist = async (req, res) => {
   const dentistId = req.params.id;
   try {
     const dentist = await Dentist.findOne({
-      where: { national_id: dentistId },
+      where: { nationalId: dentistId },
       attributes: { exclude: ['createdAt', 'updatedAt'] },
       include: [
         {
@@ -149,7 +148,7 @@ exports.getDentist = async (req, res) => {
             exclude: [
               'createdAt',
               'updatedAt',
-              'created_by',
+              'createdBy',
               'patientId',
               'dentistId',
               'transferedId',
@@ -159,7 +158,7 @@ exports.getDentist = async (req, res) => {
             {
               model: Patient,
               attributes: {
-                exclude: ['createdAt', 'updatedAt', 'created_by'],
+                exclude: ['createdAt', 'updatedAt', 'createdBy'],
               },
             },
           ],
@@ -170,7 +169,7 @@ exports.getDentist = async (req, res) => {
             exclude: [
               'createdAt',
               'updatedAt',
-              'created_by',
+              'createdBy',
               'patientId',
               'dentistId',
               'clinicId',
@@ -180,7 +179,7 @@ exports.getDentist = async (req, res) => {
             {
               model: Patient,
               attributes: {
-                exclude: ['createdAt', 'updatedAt', 'created_by'],
+                exclude: ['createdAt', 'updatedAt', 'createdBy'],
               },
             },
           ],
@@ -197,7 +196,7 @@ exports.getDentistId = async (req, res) => {
   const dentistId = req.params.id;
   try {
     const dentist = await Dentist.findOne({
-      where: { national_id: dentistId },
+      where: { nationalId: dentistId },
       attributes: ['id'],
     });
     res.status(200).json({ message: 'success', data: dentist });
@@ -209,7 +208,7 @@ exports.getDentistId = async (req, res) => {
 exports.getInDoctor = async (req, res) => {
   try {
     const dentist = await Dentist.findAll({
-      where: { works_in: 'Specialized Clinics' },
+      where: { worksIn: 'Specialized Clinics' },
       attributes: ['id', 'name'],
     });
     res.status(200).json({ message: 'success', data: dentist });
@@ -221,7 +220,7 @@ exports.getInDoctor = async (req, res) => {
 exports.getOutDoctor = async (req, res) => {
   try {
     const dentist = await Dentist.findAll({
-      where: { works_in: 'Outpatient Clinics' },
+      where: { worksIn: 'Outpatient Clinics' },
       attributes: ['id', 'name'],
     });
     res.status(200).json({ message: 'success', data: dentist });
@@ -249,7 +248,7 @@ exports.addClinic = async (req, res) => {
   if (!req.body.name) {
     return res.status(400).json({ message: 'Name is required' });
   }
-  const name = req.body.name;
+  const { name } = req.body;
   try {
     const clinic = await Clinic.create({
       name: name,
@@ -265,7 +264,7 @@ exports.addRole = async (req, res) => {
   if (!req.body.name) {
     return res.status(400).json({ message: 'Name is required' });
   }
-  const name = req.body.name;
+  const { name } = req.body;
   const nameToLower = name.toLowerCase();
   try {
     const role = await Role.create({ name: nameToLower });
@@ -277,17 +276,14 @@ exports.addRole = async (req, res) => {
 
 /* Users */
 exports.addUser = async (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
-  const roleId = req.body.roleId;
+  const { name, email, password, roleId } = req.body;
   try {
     const hashedPawwrod = await bcrypt.hash(password, 12);
     const user = await Users.create({
-      name: name,
-      email: email,
+      name,
+      email,
       password: hashedPawwrod,
-      roleId: roleId,
+      roleId,
     });
     res.status(201).json({ message: 'Added Successfully', data: user });
   } catch (err) {
@@ -297,18 +293,14 @@ exports.addUser = async (req, res) => {
 
 /* Dentists */
 exports.addDentist = async (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const phone = req.body.phone;
-  const national_id = req.body.national_id;
-  const works_in = req.body.works_in;
+  const { name, email, phone, nationalId, worksIn } = req.body;
   try {
     const dentist = await Dentist.create({
-      name: name,
-      email: email,
-      phone: phone,
-      national_id: national_id,
-      works_in: works_in,
+      name,
+      email,
+      phone,
+      nationalId,
+      worksIn,
     });
     res.status(201).json({ message: 'Added Successfully', data: dentist });
   } catch (err) {
@@ -320,7 +312,7 @@ exports.addDentist = async (req, res) => {
 /* Clinics */
 exports.editClinic = async (req, res) => {
   const clinicId = req.params.id;
-  const name = req.body.name;
+  const { name } = req.body;
   try {
     const clinic = await Clinic.findByPk(clinicId);
     if (!clinic) {
@@ -337,7 +329,7 @@ exports.editClinic = async (req, res) => {
 /* Roles */
 exports.editRole = async (req, res) => {
   const roleId = req.params.id;
-  const name = req.body.name;
+  const { name } = req.body;
   try {
     const role = await Role.findByPk(roleId);
     if (!role) {
@@ -354,9 +346,7 @@ exports.editRole = async (req, res) => {
 /* Users */
 exports.editUser = async (req, res) => {
   const userId = req.params.id;
-  const name = req.body.name;
-  const email = req.body.email;
-  const roleId = req.body.roleId;
+  const { name, email, roleId } = req.body;
   try {
     const user = await Users.findByPk(userId);
     if (!user) {
@@ -375,11 +365,7 @@ exports.editUser = async (req, res) => {
 /* Dentists */
 exports.editDentist = async (req, res) => {
   const dentistId = req.params.id;
-  const name = req.body.name;
-  const email = req.body.email;
-  const phone = req.body.phone;
-  const national_id = req.body.national_id;
-  const works_in = req.body.works_in;
+  const { name, email, phone, nationalId, worksIn } = req.body;
   try {
     const dentist = await Dentist.findByPk(dentistId);
     if (!dentist) {
@@ -388,8 +374,8 @@ exports.editDentist = async (req, res) => {
     dentist.name = name;
     dentist.email = email;
     dentist.phone = phone;
-    dentist.national_id = national_id;
-    dentist.works_in = works_in;
+    dentist.nationalId = nationalId;
+    dentist.worksIn = worksIn;
     res.status(200).json({ message: 'Updated Successfully', data: dentist });
     await dentist.save();
   } catch (err) {
