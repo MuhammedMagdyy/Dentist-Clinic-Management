1088b1da906b6a957f8cf2b2cc9e9e354b3cfd92
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-09-30 10:07:05 +0300

    Refactor: Nurse router
    - Applied validation in all endpoints.
    - Made the endpoints more readable.

--- routes/nurse.js
+++ routes/nurse.js
@@ -1,27 +1,70 @@
 const express = require('express');
+
 const router = express.Router();
 const nurseController = require('../controllers/nurse');
-const { body } = require('express-validator');
 const isAuth = require('../middlewares/is-auth');
-const { validationMiddleWare } = require('../middlewares/is-validator');
+const {
+  getPatientValidator,
+  addPatientValidator,
+  updatePatientValidator,
+  deletePatientValidator,
+} = require('../utils/validators/nurse/patient/patientValidator');
+const {
+  getOutPatientValidator,
+  addOutPatientValidator,
+  updateOutPatientValidator,
+  deleteOutPatientValidator,
+} = require('../utils/validators/nurse/outpatient/outpatientValidator');
+const {
+  getSpecializedValidator,
+  addSpecializedValidator,
+  updateSpecializedValidator,
+  deleteSpecializedValidator,
+} = require('../utils/validators/nurse/specialized/specializedValidator');
+const {
+  getAppointmentValidator,
+  addAppointmentValidator,
+  updateAppointmentValidator,
+} = require('../utils/validators/nurse/appointment/appointmentValidator');
 
 // GET
-router.get('/all-patients', isAuth, nurseController.getAllPatients);
-router.get('/patient/:id', isAuth, nurseController.getPatient);
+router.get('/patients', isAuth, nurseController.getAllPatients);
+router.get(
+  '/patients/:id',
+  isAuth,
+  getPatientValidator,
+  nurseController.getPatient
+);
 router.get('/patientId/:id', isAuth, nurseController.getPatientId);
 
-router.get('/all-outpatients', isAuth, nurseController.getAllOutPatients);
-router.get('/outpatient/:id', isAuth, nurseController.getOutPatient);
-
-router.get('/all-specialized', isAuth, nurseController.getAllSpecialized);
-router.get('/specialized/:id', isAuth, nurseController.getSpecialized);
-
-router.get('/patient-data/:id', isAuth, nurseController.getAllPatientData);
-
-router.get('/appoitment/patient/:id', isAuth, nurseController.getAppointment);
+router.get('/outpatients', isAuth, nurseController.getAllOutPatients);
 router.get(
-  '/appoitment/clinic/:id',
+  '/outpatients/:id',
   isAuth,
+  getOutPatientValidator,
+  nurseController.getOutPatient
+);
+
+router.get('/specializes', isAuth, nurseController.getAllSpecialized);
+router.get(
+  '/specializes/:id',
+  isAuth,
+  getSpecializedValidator,
+  nurseController.getSpecialized
+);
+
+router.get('/medical-history/:id', isAuth, nurseController.getAllPatientData);
+
+router.get(
+  '/appoitments/patient/:id',
+  isAuth,
+  getAppointmentValidator,
+  nurseController.getAppointment
+);
+router.get(
+  '/appoitments/clinic/:id',
+  isAuth,
+  getAppointmentValidator,
   nurseController.getAppointmentClinic
 );
 
@@ -29,83 +72,87 @@ router.get('/summary', isAuth, nurseController.getSummary);
 
 // POST
 router.post(
-  '/add-patient',
-  [
-    body('name').trim().notEmpty(),
-    body('age').trim().notEmpty(),
-    body('phone').trim().notEmpty(),
-    body('gender').trim().notEmpty(),
-    body('address').trim().notEmpty(),
-    body('city').trim().notEmpty(),
-    body('national_id').trim().notEmpty(),
-    body('nationality').trim().notEmpty(),
-    body('marital_status').trim().notEmpty(),
-    body('occupation').trim().notEmpty(),
-  ],
-  validationMiddleWare,
+  '/patients',
   isAuth,
+  addPatientValidator,
   nurseController.addPatient
 );
 
 router.post(
-  '/add-outpatient',
-  [
-    body('chief_complaint').trim().notEmpty(),
-    body('medical_history').trim().notEmpty(),
-    body('diagnosis').trim().notEmpty(),
-    body('extra_oral').trim().notEmpty(),
-    body('intra_oral').trim().notEmpty(),
-    body('upper_right').trim().notEmpty(),
-    body('upper_left').trim().notEmpty(),
-    body('down_right').trim().notEmpty(),
-    body('down_left').trim().notEmpty(),
-    body('patientId').trim().notEmpty(),
-    body('dentistId').trim().notEmpty(),
-    body('transferedId').trim().notEmpty(),
-  ],
-  validationMiddleWare,
+  '/outpatients',
   isAuth,
+  addOutPatientValidator,
   nurseController.addOutPatient
 );
 
 router.post(
-  '/add-specialized',
-  [
-    body('examination').trim().notEmpty(),
-    body('treatment').trim().notEmpty(),
-    body('patientId').trim().notEmpty(),
-    body('dentistId').trim().notEmpty(),
-    body('clinicId').trim().notEmpty(),
-    body('diagnosis').trim().notEmpty(),
-    body('radiographic_exam').trim().notEmpty(),
-    body('treatment_plant').trim().notEmpty(),
-  ],
-  validationMiddleWare,
+  '/specializes',
   isAuth,
+  addSpecializedValidator,
   nurseController.addSpecialized
 );
 
-router.post('/add-appointment', isAuth, nurseController.addAppointment);
+router.post(
+  '/appointments',
+  isAuth,
+  addAppointmentValidator,
+  nurseController.addAppointment
+);
 
 // PUT
-router.put('/patient/:id', isAuth, nurseController.editPatient);
-
-router.put('/outpatient/:id', isAuth, nurseController.editOutPatient);
-
-router.put('/specialized/:id', isAuth, nurseController.editSpecialized);
+router.put(
+  '/patients/:id',
+  isAuth,
+  updatePatientValidator,
+  nurseController.editPatient
+);
 
 router.put(
-  '/appointment/transferedTo/:id',
+  '/outpatients/:id',
+  isAuth,
+  updateOutPatientValidator,
+  nurseController.editOutPatient
+);
+
+router.put(
+  '/specializes/:id',
+  isAuth,
+  updateSpecializedValidator,
+  nurseController.editSpecialized
+);
+
+router.put(
+  '/appointments/transferedTo/:id',
   isAuth,
   nurseController.editAppointment
 );
-router.put('/appointment/status/:id', isAuth, nurseController.editStatus);
+router.put(
+  '/appointments/status/:id',
+  isAuth,
+  updateAppointmentValidator,
+  nurseController.editStatus
+);
 
 // DELETE
-router.delete('/patient/:id', isAuth, nurseController.deletePatient);
+router.delete(
+  '/patients/:id',
+  isAuth,
+  deletePatientValidator,
+  nurseController.deletePatient
+);
 
-router.delete('/outpatient/:id', isAuth, nurseController.deleteOutPatient);
+router.delete(
+  '/outpatients/:id',
+  isAuth,
+  deleteOutPatientValidator,
+  nurseController.deleteOutPatient
+);
 
-router.delete('/specialized/:id', isAuth, nurseController.deleteSpecialized);
+router.delete(
+  '/specializes/:id',
+  isAuth,
+  deleteSpecializedValidator,
+  nurseController.deleteSpecialized
+);
 
 module.exports = router;
